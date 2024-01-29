07c82b99f88122731fd6084e9c6106b1a817eddb
Lihi Shamriz <lihishamriz@gmail.com>
2024-01-29 16:58:33 +0200

    Initial web server

---
+++ Lab/Sources/Config.java
@@ -0,0 +1,33 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Config {
+    private Properties properties;
+
+    public Config(String configFile) {
+        properties = new Properties();
+        try {
+            properties.load(new FileReader(configFile));
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle the exception
+        }
+    }
+
+    public int getPort() {
+        return Integer.parseInt(properties.getProperty("port", "8080"));
+    }
+
+    public String getRoot() {
+        return properties.getProperty("root", "~/www/lab/html/");
+    }
+
+    public String getDefaultPage() {
+        return properties.getProperty("defaultPage", "index.html");
+    }
+
+    public int getMaxThreads() {
+        return Integer.parseInt(properties.getProperty("maxThreads", "10"));
+    }
+}
---
+++ Lab/Sources/WebServer.java
@@ -0,0 +1,2 @@
+public class WebServer {
+}
